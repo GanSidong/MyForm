e7c3ff8693aea223ad93101ce7cb2d22e89ec834
gansd <274359165@qq.com>
2017-10-26 14:50:18 +0800

    将项目提交

---
+++ SysForm/MyForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+namespace SysForm
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("user32.dll")]
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+        public const int WM_SYSCOMMAND = 0x0112;
+        public const int SC_MOVE = 0xF010;
+        public const int HTCAPTION = 0x0002;
+
+        private void gPanelTitleBack_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0);//调用移动无窗体控件函数
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.minBtn = new System.Windows.Forms.Button();
+            this.resBtn = new System.Windows.Forms.Button();
+            this.cloBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(200)))));
+            this.panel1.Controls.Add(this.minBtn);
+            this.panel1.Controls.Add(this.resBtn);
+            this.panel1.Controls.Add(this.cloBtn);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(301, 30);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.gPanelTitleBack_MouseDown);
+            // 
+            // minBtn
+            // 
+            this.minBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.minBtn.BackgroundImage = global::SysForm.Properties.Resources.最小化;
+            this.minBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.minBtn.FlatAppearance.BorderSize = 0;
+            this.minBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.minBtn.Location = new System.Drawing.Point(207, 3);
+            this.minBtn.Name = "minBtn";
+            this.minBtn.Size = new System.Drawing.Size(23, 23);
+            this.minBtn.TabIndex = 2;
+            this.minBtn.Tag = "0";
+            this.minBtn.UseVisualStyleBackColor = true;
+            this.minBtn.Click += new System.EventHandler(this.minBtn_Click);
+            // 
+            // resBtn
+            // 
+            this.resBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.resBtn.BackgroundImage = global::SysForm.Properties.Resources.还原;
+            this.resBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.resBtn.FlatAppearance.BorderSize = 0;
+            this.resBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.resBtn.Location = new System.Drawing.Point(236, 4);
+            this.resBtn.Name = "resBtn";
+            this.resBtn.Size = new System.Drawing.Size(23, 23);
+            this.resBtn.TabIndex = 1;
+            this.resBtn.Tag = "1";
+            this.resBtn.UseVisualStyleBackColor = true;
+            this.resBtn.Click += new System.EventHandler(this.resBtn_Click);
+            // 
+            // cloBtn
+            // 
+            this.cloBtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cloBtn.BackgroundImage = global::SysForm.Properties.Resources.关闭;
+            this.cloBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.cloBtn.FlatAppearance.BorderSize = 0;
+            this.cloBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cloBtn.Location = new System.Drawing.Point(265, 4);
+            this.cloBtn.Name = "cloBtn";
+            this.cloBtn.Size = new System.Drawing.Size(23, 23);
+            this.cloBtn.TabIndex = 0;
+            this.cloBtn.Tag = "2";
+            this.cloBtn.UseVisualStyleBackColor = true;
+            this.cloBtn.Click += new System.EventHandler(this.cloBtn_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(300, 600);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MyForm";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button minBtn;
+        private System.Windows.Forms.Button resBtn;
+        private System.Windows.Forms.Button cloBtn;
+        private Timer timer1;
+
+    }
+}
+
---
+++ SysForm/MyForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SysForm
+{
+    public partial class MyForm : Form
+    {
+        public MyForm()
+        {
+            InitializeComponent();
+        }
+
+        private void cloBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void resBtn_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void minBtn_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            System.Drawing.Point pp = new Point(Cursor.Position.X, Cursor.Position.Y);//获取鼠标在屏幕的坐标点
+            Rectangle Rects = new Rectangle(this.Left, this.Top, this.Left + this.Width, this.Top + this.Height);//存储当前窗体在屏幕的所在区域
+            if ((this.Top < 0) && Win32API.PtInRect(ref Rects, pp))//当鼠标在当前窗体内，并且窗体的Top属性小于0
+            {
+                this.Top = 0;//设置窗体的Top属性为0
+            }
+            else
+            {
+                //当窗体的上边框与屏幕的顶端的距离小于5时
+                if (this.Top > -5 && this.Top < 5 && !(Win32API.PtInRect(ref Rects, pp)))
+                {
+                    this.Top = 1 - this.Height;//将窗体隐藏到屏幕的顶端,并且留下1px的边框
+                }
+            }
+            if ((this.Left < 0) && Win32API.PtInRect(ref Rects, pp))
+            {
+                Win32API.AnimateWindow(this.Handle, 1000, Win32API.AW_BLEND);
+                this.Left = 0;
+            }
+            int ScreenWidth = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width;
+            if ((this.Right > ScreenWidth) && Win32API.PtInRect(ref Rects, pp))
+            {
+                this.Left = ScreenWidth - this.Width;
+                Win32API.AnimateWindow(this.Handle, 1000, Win32API.AW_BLEND);
+            }
+        }
+
+        class Win32API
+        {
+
+            public const Int32 AW_HOR_POSITIVE = 0x00000001; // 从左到右打开窗口  
+            public const Int32 AW_HOR_NEGATIVE = 0x00000002; // 从右到左打开窗口  
+            public const Int32 AW_VER_POSITIVE = 0x00000004; // 从上到下打开窗口  
+            public const Int32 AW_VER_NEGATIVE = 0x00000008; // 从下到上打开窗口  
+            public const Int32 AW_CENTER = 0x00000010; //若使用了AW_HIDE标志，则使窗口向内重叠；若未使用AW_HIDE标志，则使窗口向外扩展。  
+            public const Int32 AW_HIDE = 0x00010000; //隐藏窗口，缺省则显示窗口。  
+            public const Int32 AW_ACTIVATE = 0x00020000; //激活窗口。在使用了AW_HIDE标志后不要使用这个标志。  
+            public const Int32 AW_SLIDE = 0x00040000; //使用滑动类型。缺省则为滚动动画类型。当使用AW_CENTER标志时，这个标志就被忽略。  
+            public const Int32 AW_BLEND = 0x00080000; //使用淡出效果。只有当hWnd为顶层窗口的时候才可以使用此标志。  
+           
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern bool AnimateWindow(
+              IntPtr hwnd, // handle to window  
+              int dwTime, // duration of animation  
+              int dwFlags // animation type  
+              );
+
+            [DllImport("User32.dll")]
+            public static extern bool PtInRect(ref Rectangle r, Point p);
+        }
+    }
+}
